c88e7d362254f86e78ec997d064634583cdcdd86
vijay vignesh <97654626+vijayvignesh021@users.noreply.github.com>
2022-11-02 19:59:54 +0530

    vijay vignesh : Assignment 2 connect with db

--- Assignments/VijayaVignesh-TL/Assignment-2/first.py
+++ Assignments/VijayaVignesh-TL/Assignment-2/first.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect, session
+import sqlite3 as sql
 
 app = Flask(__name__,template_folder='templates',static_folder='statics')
 
@@ -14,9 +15,29 @@ def about():
 def signup():
     return render_template('flask_app/signup.html')
 
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         email = request.form['email']
+         passw = request.form['password']
+
+         
+         with sql.connect("user.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (email,password) VALUES (?,?)",(email,passw) )
+            con.commit()
+            msg = "<h1>Record successfully added!<h1>"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("flask_app/index.html",msg = msg)
+         con.close()
 @app.route("/login")
 def login():
     return render_template('flask_app/login.html')
 
 if __name__==("__main__"):
-    app.run()
+    app.run(debug=True)
